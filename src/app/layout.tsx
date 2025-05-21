637adf3b0b649473f164ea3a5d159e8c6f91dd38
Aki <y.akitaka2009@icloud.com>
2025-05-21 22:10:56 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,11 +12,6 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-// src/app/layout.tsx
-export const metadata:Metadata = {
-  title: 'NextUnion - 技術で未来をつなぐ団体',
-  description: 'NextUnionは技術提供を通じて未来を支える任意団体です。',
-};
 
 export default function RootLayout({
   children,
